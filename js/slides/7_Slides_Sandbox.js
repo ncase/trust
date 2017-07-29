5bd145debbfb496814e21eaaaa93d69b96723381
Nicky Case <n@ncase.me>
2017-07-29 14:33:28 -0400

    THE COMMA OF DOOM, REMOVED

--- js/slides/7_Slides_Sandbox.js
+++ js/slides/7_Slides_Sandbox.js
@@ -5,7 +5,7 @@ SLIDES.push({
 
 		// The tournament simulation
 		Tournament.resetGlobalVariables();
-		self.add({id:"tournament", type:"Tournament", x:-20, y:-20},);
+		self.add({id:"tournament", type:"Tournament", x:-20, y:-20});
 
 		// Screw it, just ALL of the Sandbox UI
 		self.add({id:"sandbox", type:"SandboxUI"});
